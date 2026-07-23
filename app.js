00a5c235c3514804f4ee53665811839ec594f0cf
kevinitunes-sketch <kevin.itunes@gmx.net>
2026-07-23 21:35:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ function startScanner() {
 
     const scanner = new Html5Qrcode("reader");
 
-    scanner.start(
-        { facingMode: "environment" },
-        {
-            fps: 10,
-            qrbox: 250
-        },
-        (decodedText) => {
-            alert("QR-Code: " + decodedText);
-            scanner.stop();
-        }
-    );
+   scanner.start(
+    { facingMode: "environment" },
+    {
+        fps: 10,
+        qrbox: 250
+    },
+    (decodedText) => {
 
+        scanner.stop();
+
+        document.getElementById("memberCard").classList.remove("hidden");
+        document.getElementById("memberNumber").textContent = decodedText;
+        document.getElementById("usedBeer").textContent = "0";
+        document.getElementById("remainingBeer").textContent = "2";
+
+    },
+    (errorMessage) => {
+        // Wird beim Scannen ständig aufgerufen.
+        // Absichtlich leer.
+    }
+);
 }

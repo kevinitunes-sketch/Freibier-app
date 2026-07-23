bb082709f2c3292c4abeddad174b8fbd9c62162c
kevinitunes-sketch <kevin.itunes@gmx.net>
2026-07-23 21:30:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const scannerButton = document.getElementById("startScanner");
 
-scannerButton.addEventListener("click", () => {
-    alert("Der Button funktioniert!");
-});
+scannerButton.addEventListener("click", startScanner);
+
+function startScanner() {
+
+    const scanner = new Html5Qrcode("reader");
+
+    scanner.start(
+        { facingMode: "environment" },
+        {
+            fps: 10,
+            qrbox: 250
+        },
+        (decodedText) => {
+            alert("QR-Code: " + decodedText);
+            scanner.stop();
+        }
+    );
+
+}

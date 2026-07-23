5c116879ab6a5f93af4f7574584f7db22bcf040a
kevinitunes-sketch <kevin.itunes@gmx.net>
2026-07-23 21:47:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,49 +6,25 @@ function startScanner() {
 
     const scanner = new Html5Qrcode("reader");
 
-   scanner.start(
-    { facingMode: "environment" },
-    {
-        fps: 10,
-        qrbox: 250
-    },
-    (decodedText) => {
+    scanner.start(
+        { facingMode: "environment" },
+        {
+            fps: 10,
+            qrbox: 250
+        },
+        (decodedText) => {
 
-   const scannerButton = document.getElementById("startScanner");
+            document.getElementById("memberCard").classList.remove("hidden");
+            document.getElementById("memberNumber").textContent = decodedText;
+            document.getElementById("usedBeer").textContent = "0";
+            document.getElementById("remainingBeer").textContent = "2";
 
-scannerButton.addEventListener("click", startScanner);
+            scanner.stop();
 
-function startScanner() {
+        },
+        (errorMessage) => {
+            // Absichtlich leer.
+        }
+    );
 
-    const scanner = new Html5Qrcode("reader");
-
-   scanner.start(
-    { facingMode: "environment" },
-    {
-        fps: 10,
-        qrbox: 250
-    },
-    (decodedText) => {
-
-        scanner.stop();
-
-        document.getElementById("memberCard").classList.remove("hidden");
-        document.getElementById("memberNumber").textContent = decodedText;
-        document.getElementById("usedBeer").textContent = "0";
-        document.getElementById("remainingBeer").textContent = "2";
-
-    },
-    (errorMessage) => {
-        // Wird beim Scannen ständig aufgerufen.
-        // Absichtlich leer.
-    }
-);
-}
-
-    },
-    (errorMessage) => {
-        // Wird beim Scannen ständig aufgerufen.
-        // Absichtlich leer.
-    }
-);
 }
